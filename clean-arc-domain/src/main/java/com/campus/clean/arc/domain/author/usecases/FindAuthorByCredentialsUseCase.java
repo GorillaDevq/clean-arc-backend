30a94b501bba400472f43cec18df10cafa4e8770
gorilladev <gorilladevq@gmail.com>
2024-05-26 19:04:50 +0300

    feat: Author-Domain useCase findById/CredentialsAuthor

---
+++ clean-arc-domain/src/main/java/com/campus/clean/arc/domain/author/usecases/FindAuthorByCredentialsUseCase.java
@@ -0,0 +1,19 @@
+package com.campus.clean.arc.domain.author.usecases;
+
+import com.campus.clean.arc.domain.author.entity.AuthorEntity;
+import com.campus.clean.arc.domain.author.port.AuthorRepository;
+import com.rcore.domain.commons.usecase.UseCase;
+import com.rcore.domain.commons.usecase.model.IdInputValues;
+import com.rcore.domain.commons.usecase.model.SingletonOptionalEntityOutputValues;
+import lombok.*;
+
+@RequiredArgsConstructor
+public class FindAuthorByCredentialsUseCase
+        extends UseCase<IdInputValues<String>, SingletonOptionalEntityOutputValues<AuthorEntity>> {
+    private final AuthorRepository authorRepository;
+
+    @Override
+    public SingletonOptionalEntityOutputValues<AuthorEntity> execute(IdInputValues<String> stringIdInputValues) {
+        return SingletonOptionalEntityOutputValues.of(authorRepository.findByCredentialId(stringIdInputValues.getId()));
+    }
+}
