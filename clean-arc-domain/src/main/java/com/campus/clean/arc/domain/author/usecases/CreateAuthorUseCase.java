41ba8d5a21685a5d17b7c09b07e5f5676b316de9
gorilladev <gorilladevq@gmail.com>
2024-05-26 18:18:27 +0300

    feat: Author-Domain useCase createAuthor

--- clean-arc-domain/src/main/java/com/campus/clean/arc/domain/author/usecases/CreateAuthorUseCase.java
+++ clean-arc-domain/src/main/java/com/campus/clean/arc/domain/author/usecases/CreateAuthorUseCase.java
@@ -10,12 +10,16 @@ import lombok.*;
 import ru.foodtechlab.lib.auth.integration.core.credential.CredentialServiceFacade;
 import ru.foodtechlab.lib.auth.integration.core.role.RoleServiceFacade;
 import ru.foodtechlab.lib.auth.integration.core.roleAccess.RoleAccessServiceFacade;
+import ru.foodtechlab.lib.auth.service.facade.credential.dto.requests.CreateCredentialRequest;
 import ru.foodtechlab.lib.auth.service.facade.credential.dto.responses.CredentialResponse;
+import ru.foodtechlab.lib.auth.service.facade.role.dto.requests.CreateRoleRequest;
 import ru.foodtechlab.lib.auth.service.facade.role.dto.responses.RoleResponse;
+import ru.foodtechlab.lib.auth.service.facade.roleAccess.dto.requests.CreateRoleAccessRequest;
 import ru.foodtechlab.lib.auth.service.facade.roleAccess.dto.requests.FindRoleAccessWithFiltersRequest;
 import ru.foodtechlab.lib.auth.service.facade.roleAccess.dto.responses.RoleAccessResponse;
 
 import javax.security.auth.login.CredentialNotFoundException;
+import java.time.Instant;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -36,12 +40,40 @@ public class CreateAuthorUseCase extends UseCase<CreateAuthorUseCase.InputValues
         } catch (Exception ignore) {}
 
         if (roleResponse == null) {
-            FindRoleAccessWithFiltersRequest req = FindRoleAccessWithFiltersRequest.builder()
-                    .limit(1000l)
-                    .build();
+            FindRoleAccessWithFiltersRequest req = FindRoleAccessWithFiltersRequest.builder().limit(1000l).build();
             List<RoleAccessResponse> accessResponseList = roleAccessServiceFacade.find(req).getItems();
+            RoleAccessResponse roleAccessResponse = null;
 
+            for (RoleAccessResponse response : accessResponseList) {
+                if(response.getIsDeleted() == false
+                        && response.getMethod().equals(RoleAccessResponse.Method.ANY)
+                        && response.getServiceName().equals("*")
+                        && response.getRequestPathPattern().equals("/**")) {
+                    roleAccessResponse = response;
+                }
+            }
+
+            if (roleAccessResponse == null) {
+                CreateRoleAccessRequest accessRequest = CreateRoleAccessRequest.builder()
+                        .serviceName("*")
+                        .requestPathPattern("/**")
+                        .method(RoleAccessResponse.Method.ANY)
+                        .build();
+
+                roleAccessResponse = roleAccessServiceFacade.create(accessRequest);
+            }
+
+            CreateRoleRequest createRoleRequest = CreateRoleRequest.builder()
+                    .name("Author Role")
+                    .code(roleCode)
+                    .accessIds(List.of(roleAccessResponse.getId()))
+                    .isRegistrationAllowed(true)
+                    .build();
+
+            roleResponse = roleServiceFacade.create(createRoleRequest);
         }
+
+        return roleResponse;
     }
 
     @Override
@@ -52,7 +84,40 @@ public class CreateAuthorUseCase extends UseCase<CreateAuthorUseCase.InputValues
             if (response != null) throw new AuthorAlreadyExistException();
         } catch (CredentialNotFoundException ignore) {}
 
-        RoleResponse roleResponse;
+        RoleResponse roleResponse = buildRole();
+
+        CreateCredentialRequest.Role role = new CreateCredentialRequest.Role(
+                roleResponse.getId(),
+                roleResponse.getCode(),
+                roleResponse.getIsDeleted()
+        );
+
+        CreateCredentialRequest createCredentialRequest = CreateCredentialRequest.builder()
+                .username(inputValues.getEmail())
+                .password(inputValues.getPassword())
+                .phoneNumber(null)
+                .email(new CredentialResponse.Email(inputValues.getEmail(), false))
+                .roles(List.of(role))
+                .isBlocked(false)
+                .confirmationCodeDestinationType(CreateCredentialRequest.ConfirmationCodeDestinationType.EMAIL)
+                .personalConfirmationCode(null)
+                .confirmationCodeType(CreateCredentialRequest.Type.ONE_TIME )
+                .build();
+
+        response = credentialServiceFacade.create(createCredentialRequest);
+
+        AuthorEntity authorEntity = AuthorEntity.builder()
+                .id(authorIdGenerator.generate())
+                .email(inputValues.getEmail())
+                .firstName(inputValues.getFirstName())
+                .lastName(inputValues.getLastName())
+                .credentialId(response.getId())
+                .createdAt(Instant.now())
+                .createdAt(Instant.now())
+                .build();
+
+        authorEntity = authorRepository.save(authorEntity);
+        return SingletonEntityOutputValues.of(authorEntity);
     }
 
     @AllArgsConstructor(staticName = "of")
